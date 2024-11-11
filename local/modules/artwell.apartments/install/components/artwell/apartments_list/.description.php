7b6b91294e50102aaef2a5b781a3f57ba0562fac
zyYr97 <46669730+zyYr97@users.noreply.github.com>
2024-11-12 00:09:24 +0300

    Upload .description.php

---
+++ local/modules/artwell.apartments/install/components/artwell/apartments_list/.description.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    'NAME' => 'Квартиры в продаже',
+    'DESCRIPTION' => 'Выводит список квартир, находящихся в продаже',
+    'PATH' => [
+        'ID' => 'artwell_components',
+        'NAME' => 'Компоненты Artwell',
+    ],
+];
