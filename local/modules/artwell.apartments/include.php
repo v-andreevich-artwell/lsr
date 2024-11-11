6f9ae7477a1b5bbcdf0cbc9166194254b15dadd6
zyYr97 <46669730+zyYr97@users.noreply.github.com>
2024-11-11 23:59:39 +0300

    Upload include.php

---
+++ local/modules/artwell.apartments/include.php
@@ -0,0 +1,5 @@
+<?php
+\Bitrix\Main\Loader::registerAutoLoadClasses('artwell.apartments', [
+    'Artwell\Apartments\ApartmentTable' => 'lib/apartmenttable.php',
+    'Artwell\Apartments\BuildingTable' => 'lib/buildingtable.php',
+]);
